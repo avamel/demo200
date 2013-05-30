8c2b561ffb1dad67c081d0292fc0d1d4dcd0d1d7
avamel <xnuamx@gmail.com>
2013-05-31 01:14:52 +0300

    setting vagrant 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/home/vagrant/demo200"
 set :scm, :git
 set :scm_user, "avamel"
 set :repository,  "https://github.com/avamel/demo200.git"
-set :branch "vagrant"
+set :branch, "vagrant"
 
 set :ssh_options, {:forward_agent => true, keys: ['~/.vagrant.d/insecure_private_key']} 
 set :default_run_options, {:pty => true}
