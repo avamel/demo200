e64df069620a727e0619afe20f7a7dd50a38e5b1
avamel <xnuamx@gmail.com>
2013-05-31 01:01:34 +0300

    vagrant setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,17 @@
-server "192.168.33.10", :app, :web, :db, :primary => true
+server "33.33.33.100", :app, :web, :db, :primary => true
 
 set :application, "demo200"
 set :user, "vagrant"
 set :deploy_to, "/home/vagrant/demo200"
 
 set :scm, :git
+set :scm_user, "avamel"
 set :repository,  "https://github.com/avamel/demo200.git"
+set :branch "vagrant"
 
 set :ssh_options, {:forward_agent => true, keys: ['~/.vagrant.d/insecure_private_key']} 
 set :default_run_options, {:pty => true}
-set :stages, ["vagrant"]
-set :default_stage, "vagrant"
+
 set :use_sudo, false
 
 
