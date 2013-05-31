14fe1516551d5cc8717ce12312e081932ffe8b1e
avamel <xnuamx@gmail.com>
2013-05-31 16:01:40 +0300

    change deploy file 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,7 @@ after "deploy:bundle_gems", "deploy:restart"
 
 namespace :deploy do
 	task :bundle_gems do
-		run "cd #{deploy_to}/current && 
-		PATH=/home/vagrant/.rvm/bin/bundle install vendor/gems"
+		run "cd #{deploy_to}/current && /home/vagrant/.rvm/bin/bundle install vendor/gems"
 	end
 	task :start do ; end 
 	task :stop do ; end
