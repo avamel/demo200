b14e179cfd0be696b064675d14e04f9a6d4dc144
avamel <xnuamx@gmail.com>
2013-05-31 16:58:53 +0300

    change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "bundler/capistrano"
+
 server "33.33.33.100", :app, :web, :db, :primary => true
 
 set :application, "demo200"
@@ -14,13 +16,13 @@ set :default_run_options, {:pty => true}
 
 set :use_sudo, false
 
-after "deploy", "deploy:bundle_gems"
-after "deploy:bundle_gems", "deploy:restart"
+after "deploy", "deploy:restart"# "deploy:bundle_gems"
+#after "deploy:bundle_gems", "deploy:restart"
 
 namespace :deploy do
-	task :bundle_gems do
-		run "cd #{deploy_to}/current && /home/vagrant/.rvm/gems/ruby-2.0.0-p195/bin/bundle install vendor/gems"
-	end
+#	task :bundle_gems do
+#		run "cd #{deploy_to}/current && /home/vagrant/.rvm/gems/ruby-2.0.0-p195/bin/bundle install vendor/gems"
+#	end
 	task :start do ; end 
 	task :stop do ; end
 	task :restart, :roles => :app, :except => { :no_release => true } do
