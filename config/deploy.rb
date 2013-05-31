5189ba2f7317d22c528cf8e2a15145ba31e8474c
avamel <xnuamx@gmail.com>
2013-05-31 14:30:15 +0300

    autorestert2

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ server "33.33.33.100", :app, :web, :db, :primary => true
 set :application, "demo200"
 set :user, "vagrant"
 set :deploy_to, "/home/vagrant/demo200"
-
+set :deploy_via, :remote_cache
 set :scm, :git
 set :scm_user, "avamel"
 set :repository,  "https://github.com/avamel/demo200.git"
@@ -16,16 +16,17 @@ set :default_run_options, {:pty => true}
 
 set :use_sudo, false
 
+after "deploy", "deploy:cleanup"
 namespace :deploy do
-  desc <<-DESC
-   
-  DESC
-  task :default do
-    update
-    restart
-  end
-end
+	task :restart, roles: :app do
+		run "toch #{current_path}/tmp/restart.txt"
+	end
 
+	task :symlink_config, roles: :app do
+		run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+	end
+	after "deploy:finalize_update", "deploy:symlink_config" 
+end
 
 
 
